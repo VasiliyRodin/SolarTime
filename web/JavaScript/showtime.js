e1ca5c61d1751a1a835131429101d5cdefa2013b
VasiliyRodin <rodinvasiliy@gmail.com>
2014-07-21 07:04:26 -0700

    Added get time.js

---
+++ web/JavaScript/showtime.js
@@ -0,0 +1,23 @@
+function checkTime(i){
+    if(i<10){
+        i="0"+i;
+    }
+    return i;
+}
+
+function startTime(){
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinuites();
+    var s = today.getSeconds();
+    
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function(){
+        startTime()
+    },500);
+}
+startTime();
+
+
