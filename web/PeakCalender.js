8438a3aa783f021b540dccd2ef0f8b038abc077c
VasiliyRodin <rodinvasiliy@gmail.com>
2014-07-26 21:56:49 -0700

    Replaced getElementbyID with local variable

--- web/PeakCalender.js
+++ web/PeakCalender.js
@@ -17,50 +17,51 @@ function changeColor(){
 
 
 function checkMonthHour(month,day,date,hour){               //month MONTH, day DAY, date DATE, hour HOUR
+    var color;
     if((month == 2 && date >= 9) || (month == 3 && date <= 5)){ // make sure in between march9 and april 5
         if(day!=0 || day!=6){ //make sure not weekend
             if(hour >= 17 && hour < 20) // hours of day
             {
-                document.getElementById("boxColor").style.backgroundColor = "yellow";//DISPLAY YELLOW
+                color = "yellow";//DISPLAY YELLOW
             }
             else{
-                document.getElementById("boxColor").style.backgroundColor = "red";// display red
+                color = "red";// display red
             }
         }
         else{
-            document.getElementById("boxColor").style.backgroundColor = "red";//display red
+            color = "red";//display red
         }
     }
     
     if((month == 3 && date >=6)){//april 6-april 30
         if(day!=0 || day!=6){//ADD HOLIDAYS LATER
             if(hour >= 16 && hour<19){//hours of day
-                document.getElementById("boxColor").style.backgroundColor = "yellow";//DISPLAY YELLOW
+                color = "yellow";//DISPLAY YELLOW
             }
         }
         else{
-            document.getElementById("boxColor").style.backgroundColor = "red";//display red
+            color = "red";//display red
         }
     
     }
     if((month == 4 && date >=1) || (month==9 && date <= 25)){
         if(day!=0 || day!=6){
             if(hour >= 1 && hour>18){
-                document.getElementById("boxColor").style.backgroundColor = "green";//display green
+                color = "green";//display green
             }
             if((hour >= 9 && hour<12) || (hour>=18 && hour<20)){
-                document.getElementById("boxColor").style.backgroundColor = "yellow";//display yellow
+                color = "yellow";//display yellow
             }
             else{
-                document.getElementById("boxColor").style.backgroundColor = "red";//DISPLAY RED
+                color = "red";//DISPLAY RED
             }
         }
         else if(day==0 || day==8){
             if((hour >= 16 && hour<19)){
-                document.getElementById("boxColor").style.backgroundColor = "yellow";//display yellow
+                color = "yellow";//display yellow
             }
             else{
-                document.getElementById("boxColor").style.backgroundColor = "red";//display red
+                color = "red";//display red
             }
         }
     }
@@ -68,21 +69,21 @@ function checkMonthHour(month,day,date,hour){               //month MONTH, day D
     if((month == 9 && date >=26)){
         if(day!=0 || day!=6){
             if(hour >= 13 && hour>19){
-                document.getElementById("boxColor").style.backgroundColor = "green";//display green
+                color = "green";//display green
             }
             if((hour >= 10 && hour<13) || (hour>=19 && hour<21)){
-                document.getElementById("boxColor").style.backgroundColor = "yellow";//display yellow
+                color = "yellow";//display yellow
             }
             else{
-                document.getElementById("boxColor").style.backgroundColor = "red";//DISPLAY RED
+                color = "red";//DISPLAY RED
             }
         }
         else if(day==0 || day==8){
             if((hour >= 17 && hour<20)){
-                document.getElementById("boxColor").style.backgroundColor = "yellow";//display yellow
+                color = "yellow";//display yellow
             }
             else{
-                document.getElementById("boxColor").style.backgroundColor = "red";//display red
+                color = "red";//display red
             }
         }
     }
@@ -91,16 +92,21 @@ function checkMonthHour(month,day,date,hour){               //month MONTH, day D
         if(day!=0 || day!=6){ //make sure not weekend
             if(hour >= 17 && hour < 20) // hours of day
             {
-                document.getElementById("boxColor").style.backgroundColor = "yellow"//DISPLAY YELLOW
+                color = "yellow"//DISPLAY YELLOW
             }
             else{
-                document.getElementById("boxColor").style.backgroundColor = "red"//display red
+                color = "red"//display red
             }
         }
         else{
-            document.getElementById("boxColor").style.backgroundColor = "red"//display red
+            color = "red"//display red
         }
     }
+    
+    
+    
+    document.getElementById("boxColor").style.backgroundColor = color;
+}
 /************************ YEAR ENDS THIS IS FOR NEXT YEAR*********** 
     if((month >= 0 && date >= 1) && (month <= 2 && date <= 7)){
         if(day!=0 || day!=6){ //make sure not weekend
@@ -117,5 +123,5 @@ function checkMonthHour(month,day,date,hour){               //month MONTH, day D
         }
     }
 */
-}
+
     
