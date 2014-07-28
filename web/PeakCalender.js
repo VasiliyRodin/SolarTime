b4dfb3cba93886603124c15eb51a11742750131d
VasiliyRodin <rodinvasiliy@gmail.com>
2014-07-27 17:12:04 -0700

    Fixed correct months

--- web/PeakCalender.js
+++ web/PeakCalender.js
@@ -19,7 +19,7 @@ function changeColor(){
 function checkMonthHour(month,day,date,hour){               //month MONTH, day DAY, date DATE, hour HOUR
     var color;
     if((month == 2 && date >= 9) || (month == 3 && date <= 5)){ // make sure in between march9 and april 5
-        if(day!=0 || day!=6){ //make sure not weekend
+        if(day!=0 && day!=6){ //make sure not weekend
             if(hour >= 18 && hour < 21) // hours of day
             {
                 color = "yellow";//DISPLAY YELLOW
@@ -34,7 +34,7 @@ function checkMonthHour(month,day,date,hour){               //month MONTH, day D
     }
     
     if((month == 3 && date >=6)){//april 6-april 30
-        if(day!=0 || day!=6){//ADD HOLIDAYS LATER
+        if(day!=0 && day!=6){//ADD HOLIDAYS LATER
             if(hour >= 17 && hour<20){//hours of day
                 color = "yellow";//DISPLAY YELLOW
             }
@@ -44,8 +44,8 @@ function checkMonthHour(month,day,date,hour){               //month MONTH, day D
         }
     
     }
-    if((month == 4 && date >=1) || (month==9 && date <= 25)){
-        if(day!=0 || day!=6){
+    if((month >= 4 && month <9) || (month==9 && date <= 25)){
+        if(day!=0 && day!=6){
             if(hour >= 13 && hour<19){
                 color = "red";//display red
             }
@@ -56,7 +56,7 @@ function checkMonthHour(month,day,date,hour){               //month MONTH, day D
                 color = "green";//DISPLAY RED
             }
         }
-        else if(day==0 || day==6){
+        else{
             if((hour >= 17 && hour<20)){
                 color = "yellow";//display yellow
             }
@@ -67,7 +67,7 @@ function checkMonthHour(month,day,date,hour){               //month MONTH, day D
     }
     
     if((month == 9 && date >=26)){
-        if(day!=0 || day!=6){
+        if(day!=0 && day!=6){
             if(hour >= 14 && hour<20){
                 color = "red";//display red
             }
@@ -78,7 +78,7 @@ function checkMonthHour(month,day,date,hour){               //month MONTH, day D
                 color = "green";//DISPLAY RED
             }
         }
-        else if(day==0 || day==8){
+        else{
             if((hour >= 18 && hour<21)){
                 color = "yellow";//display yellow
             }
@@ -88,8 +88,8 @@ function checkMonthHour(month,day,date,hour){               //month MONTH, day D
         }
     }
         
-    if((month == 10) || (month <= 11)){
-        if(day!=0 || day!=6){ //make sure not weekend
+    if((month >= 10)){
+        if(day!=0 && day!=6){ //make sure not weekend
             if(hour >= 18 && hour < 21) // hours of day
             {
                 color = "yellow"//DISPLAY YELLOW
