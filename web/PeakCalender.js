3b3a64936b6f11d920914775ba71b4d8fe58c449
VasiliyRodin <rodinvasiliy@gmail.com>
2014-08-13 21:08:14 -0700

    Added period end time as a object

--- web/PeakCalender.js
+++ web/PeakCalender.js
@@ -11,193 +11,147 @@ function changeColor(){
     var day = d.getDay();
     var date = d.getDate();
     var hour = d.getHours();
-    var color = checkMonthHour(month,day,date,hour);
-    var status = getStatus(color);
-    var endTime = getEndTime(month,day,date,hour);
-    document.getElementById("boxColor").style.backgroundColor = color;
+    var result = checkMonthHour(month,day,date,hour);
+    var status = getStatus(result.color);
+    document.getElementById("boxColor").style.backgroundColor = result.color;
     document.getElementById("status").innerHTML = status;
-    document.getElementById("peakEnd").innerHTML = endTime;
+    document.getElementById("peakEnd").innerHTML = result.endTime;
 }
 
+function isWeekend(day){
+    return day == 0 || day ==6;
+}
 
-function checkMonthHour(month,day,date,hour){               //month MONTH, day DAY, date DATE, hour HOUR
+function checkMonthHour(month,day,date,hour){               
     var color;
-    if((month == 2 && date >= 9) || (month == 3 && date <= 5)){ // make sure in between march9 and april 5
-        if(day!=0 && day!=6){ //make sure not weekend
-            if(hour >= 18 && hour < 21) // hours of day
-            {
-                color = "yellow";//DISPLAY YELLOW
-            }
-            else{
-                color = "green";// display green
-            }
-        }
-        else{
-            color = "green";//display green
-        }
-    }
-    
-    if((month == 3 && date >=6)){//april 6-april 30
-        if(day!=0 && day!=6){//ADD HOLIDAYS LATER
-            if(hour >= 17 && hour<20){//hours of day
-                color = "yellow";//DISPLAY YELLOW
-            }
-        }
-        else{
-            color = "green";//display green
-        }
-    
-    }
-    if((month >= 4 && month <9) || (month==9 && date <= 25)){
-        if(day!=0 && day!=6){
-            if(hour >= 13 && hour<19){
-                color = "red";//display red
-            }
-            else if((hour >= 10 && hour<13) || (hour>=19 && hour<21)){
-                color = "yellow";//display yellow
-            }
-            else{
-                color = "green";//DISPLAY RED
-            }
-        }
-        else{
-            if((hour >= 17 && hour<20)){
-                color = "yellow";//display yellow
-            }
-            else{
-                color = "green";//display green
-            }
-        }
-    }
-    
-    if((month == 9 && date >=26)){
-        if(day!=0 && day!=6){
-            if(hour >= 14 && hour<20){
-                color = "red";//display red
-            }
-            else if((hour >= 11 && hour<14) || (hour>=20 && hour<22)){
-                color = "yellow";//display yellow
-            }
-            else{
-                color = "green";//DISPLAY RED
-            }
-        }
-        else{
-            if((hour >= 18 && hour<21)){
-                color = "yellow";//display yellow
-            }
-            else{
-                color = "green";//display green
-            }
-        }
-    }
-        
-    if((month >= 10)){
-        if(day!=0 && day!=6){ //make sure not weekend
-            if(hour >= 18 && hour < 21) // hours of day
-            {
-                color = "yellow"//DISPLAY YELLOW
-            }
-            else{
-                color = "green"//display green
-            }
-        }
-        else{
-            color = "green"//display green
-        }
-    }
-    
-    return color;
-}
-
-function getEndTime(month,day,date,hour){               //month MONTH, day DAY, date DATE, hour HOUR
     var endTime;
-    if((month == 2 && date >= 9) || (month == 3 && date <= 5)){ // make sure in between march9 and april 5
-        if(day!=0 && day!=6){ //make sure not weekend
-            if(hour >= 18 && hour < 21) // hours of day
-            {
-                endTime = "11:00 pm";//DISPLAY YELLOW
+    //between march 9 and april 5 OR November 1 - December 31
+    if((month == 2 && date >= 9) || (month == 3 && date <= 5) || (month >= 10)) { 
+        if(!isWeekend(day)) {
+            if(hour >= 18 && hour < 21) {
+                color = "yellow";
+                endTime = "9:00pm";
+            } else {
+                color = "green";
+                if(hour < 18) { 
+                    endTime = "6:00pm";
+                } else if(hour >= 21) {
+                    if(day == 5) {
+                        endTime = "6:00pm next Monday"
+                    } else {
+                        endTime = "6:00pm Tomorrow";
+                    }
+                }
             }
-            else{
-                endTime = "6:00 pm";// display green
-            }
-        }
-        else{
-            endTime = "Monday";//display green
+        } else {
+            color = "green";
+            endTime = "6:00pm Monday";
         }
     }
     
-    if((month == 3 && date >=6)){//april 6-april 30
-        if(day!=0 && day!=6){//ADD HOLIDAYS LATER
-            if(hour >= 17 && hour<20){//hours of day
-                endTime = "10:00 pm";//DISPLAY YELLOW
+    //april 6-april 30
+    if((month == 3 && date >=6)) {
+        if(!isWeekend(day)) {
+            if(hour >= 17 && hour<20){
+                color = "yellow";
+                endTime = "8:00 pm";
+            } else {
+                color = "green";
+                if(hour < 17) { 
+                    endTime = "5:00pm";
+                } else if(hour >= 20) {
+                    if(day == 5) {
+                        endTime = "5:00pm next Monday"
+                    } else {
+                        endTime = "5:00pm Tomorrow";
+                    }
+                }
             }
-        }
-        else{
-            endTime = "Monday";//display green
-        }
-    
+        } else{
+            color = "green";
+            endTime = "5:00pm Monday";
+        }    
     }
+    
+    // may 1 - october 25
     if((month >= 4 && month <9) || (month==9 && date <= 25)){
-        if(day!=0 && day!=6){
+        if(!isWeekend(day)) {
             if(hour >= 13 && hour<19){
-                endTime = "7:00 pm";//display red
+                color = "red";
+                endTime = "7:00pm"
+            } else if(hour >= 10 && hour < 13){
+                color = "yellow";
+                endTime = "1:00pm";
+            } else if(hour >= 19 && hour < 21) {
+                color = "yellow";
+                endTime = "9:00pm";
+            } else {
+                color = "green";
+                if(day != 5) {
+                    endTime = "10:00am Tomorrow";
+                } else {
+                    endTime = "5:00pm Tomorrow";
+                }
             }
-            else if((hour >= 10 && hour<13) || (hour>=19 && hour<21)){
-                endTime = "1:00 pm";//display yellow
-            }
-            else{
-                endTime = "10:00 am";//DISPLAY RED
-            }
-        }
-        else{
+        } else {
             if((hour >= 17 && hour<20)){
-                endTime = "10:00 am";//display yellow
-            }
-            else{
-                endTime = "5:00 pm";//display green
+                color = "yellow";
+                endTime = "8:00pm";
+            } else {
+                color = "green";
+                if(hour < 17) {
+                    endTime = "5:00pm";
+                } else {
+                    if(day == 0) {
+                        endTime = "10:00am Tomorrow";
+                    } else {
+                        endTime = "5:00pm Tomorrow";
+                    }
+                }
             }
         }
     }
     
+    //october 26-31
     if((month == 9 && date >=26)){
-        if(day!=0 && day!=6){
+                if(!isWeekend(day)) {
             if(hour >= 14 && hour<20){
-                endTime = "8:00 pm";//display red
+                color = "red";
+                endTime = "8:00pm"
+            } else if(hour >= 11 && hour < 14){
+                color = "yellow";
+                endTime = "2:00pm";
+            } else if(hour >= 20 && hour < 22) {
+                color = "yellow";
+                endTime = "10:00pm";
+            } else {
+                color = "green";
+                if(day != 5) {
+                    endTime = "11:00am Tomorrow";
+                } else {
+                    endTime = "6:00pm Tomorrow";
+                }
             }
-            else if((hour >= 11 && hour<14) || (hour>=20 && hour<22)){
-                endTime = "2:00 pm";//display yellow
-            }
-            else{
-                endTime = "11:00 am";//DISPLAY RED
-            }
-        }
-        else{
+        } else {
             if((hour >= 18 && hour<21)){
-                endTime = "11:00 pm";//display yellow
+                color = "yellow";
+                endTime = "9:00pm";
+            } else {
+                color = "green";
+                if(hour < 18) {
+                    endTime = "6:00pm";
+                } else {
+                    if(day == 0) {
+                        endTime = "11:00am Tomorrow";
+                    } else {
+                        endTime = "6:00pm Tomorrow";
+                    }
+                }
             }
-            else{
-                endTime = "6:00 pm";//display green
-            }
-        }
-    }
-        
-    if((month >= 10)){
-        if(day!=0 && day!=6){ //make sure not weekend
-            if(hour >= 18 && hour < 21) // hours of day
-            {
-                endTime = "11:00 pm"//DISPLAY YELLOW
-            }
-            else{
-                endTime = "6:00 pm"//display green
-            }
-        }
-        else{
-            endTime = "Monday"//display green
         }
     }
     
-    return endTime;
+    return {color:color,endTime:endTime};
 }
 
 function getStatus(color){
@@ -213,39 +167,3 @@ function getStatus(color){
     }
     return status;
 }
-
-function getStatus(color){
-    var status;
-    if(color == "green"){
-        status = "Off-Peak (Ok to use electricity).";
-    }
-    else if (color == "red"){
-        status = "On-Peak (Try to conserve electricity).";
-    }
-    else{
-        status = "Partial Peak";
-    }
-    return status;
-}
-
-
-
-
-/************************ YEAR ENDS THIS IS FOR NEXT YEAR*********** 
-    if((month >= 0 && date >= 1) && (month <= 2 && date <= 7)){
-        if(day!=0 || day!=6){ //make sure not weekend
-            if(hour >= 17 && hour < 20) // hours of day
-            {
-                //DISPLAY YELLOW
-            }
-            else{
-                //display green
-            }
-        }
-        else{
-            //display green
-        }
-    }
-*/
-
-    
